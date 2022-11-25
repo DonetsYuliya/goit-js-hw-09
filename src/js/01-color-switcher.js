b5785145c98f970a4dccb5b3d37fb52dec7ecd34
DonetsYuliya <donecjuli@gmail.com>
2022-11-25 10:36:13 +0200

    task 01 of hw-09

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const bodyEl = document.querySelector('body');
+const startEl = document.querySelector('button[data-start]');
+const stopEl = document.querySelector('button[data-stop]');
 
+let intervalId = null;
+
+const toChangeColorBody = () => {
+  intervalId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startEl.toggleAttribute('disabled');
+};
+
+const toStopColorChanged = () => {
+  clearInterval(intervalId);
+  startEl.toggleAttribute('disabled');
+};
+
+startEl.addEventListener('click', toChangeColorBody);
+stopEl.addEventListener('click', toStopColorChanged);
